b3ab737e8d54cc796228937501650321cc38236b
2DevGs <dev.gustavoserrano@gmail.com>
2025-06-25 16:38:22 -0300

    Initial setup and navigation
    This commit implemented a navigation structure named simple, using the named route to navigate between pages.

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatelessWidget {
+
+  const HomePage({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('HOME PAGE'),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: (){
+                Get.offNamed('/initial/page1');
+              },
+              child: Text('Initial'),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
